2a22998f3edb9ad8553b9478ef069be15a07c8a4
arash arjmand <arash@arashs-MacBook-Pro.local>
2020-11-04 19:05:12 +0330

    addedtable view, costum protocla in the model, displaying video titles in the tableview

--- youtube-onedaybuild/Model.swift
+++ youtube-onedaybuild/Model.swift
@@ -7,7 +7,15 @@
 
 import Foundation
 
+protocol ModelDelegate {
+    
+    func videosFetched(_ videos: [Video])
+    
+}
+
 class Model {
+    
+    var delegate: ModelDelegate?
     
     func getVideos() {
         
@@ -36,6 +44,16 @@ class Model {
                 decoder.dateDecodingStrategy = .iso8601
                 
                 let response = try decoder.decode(Response.self, from: data!)
+                
+                if response.items != nil {
+                    
+                    DispatchQueue.main.async {
+                        
+                        // Call the "videosFetched" method for the delegate
+                        self.delegate?.videosFetched(response.items!)
+                        
+                    }
+                }
                 
                 dump(response)
             }
